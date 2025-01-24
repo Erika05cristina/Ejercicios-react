f48e6ceaeff9173bad385ef266caf3f0c446a2cb
Erika05cristina <evillaq1@est.ups.edu.ec>
2025-01-24 11:29:26 -0500

    fit: 1.78 unicafe, step 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,55 @@
 import { useState } from 'react'
 
-const App = () => {
-  // guarda los clics de cada botón en su propio estado
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
-  const all = good + neutral + bad
-  const average = (good - bad) / all
-  const positive = good / all * 100
+const Statistics = ({ good, neutral, bad }) => {
+    const all = good + neutral + bad
+    const average = (good - bad) / all
+    const positive = good / all * 100
+    return (
+      <div>
+        <p>All: {all}</p>
+        <p>Average: {average}</p>
+        <p>Positive: {positive} %</p>
+      </div>
+    )
+  }
+  
+  
+  const App = () => {
+    // guarda los clics de cada botón en su propio estado
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+    
+    const handleGoodClick = () => {
+      setGood(good + 1);
+      console.log('Good:', good + 1);
+    };
+  
+    const handleNeutralClick = () => {
+      setNeutral(neutral + 1);
+      console.log('Neutral:', neutral + 1);
+    };
+  
+    const handleBadClick = () => {
+      setBad(bad + 1);
+      console.log('Bad:', bad + 1);
+    };
+  
+  
 
   return ( 
     <div>
         <h1>Give Feedback</h1>
-        <button onClick={() => { setGood(good + 1); console.log('Good:', good + 1); }}>Good</button>
-        <button onClick={() => { setNeutral(neutral + 1); console.log('Neutral:', neutral + 1); }}>Neutral</button>
-        <button onClick={() => { setBad(bad + 1); console.log('Bad:', bad + 1); }}>Bad</button>
-
-        <h2>Statistics</h2>
-        <p>good {good}</p>
+        <button onClick={handleGoodClick}>Good</button>
+        <button onClick={handleNeutralClick}>Neutral</button>
+        <button onClick={handleBadClick}>Bad</button>
+        <p>good {good }</p>
         <p>neutral {neutral}</p>
         <p>bad {bad}</p>
-        <p>-----------------------------------</p>
-        <p>all {all}</p>
-        <p>average {average}</p>
-        <p>positive {positive} %</p>
 
+        <p>-----------------------------------</p>
+        <h2>Statistics</h2>
+        <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
