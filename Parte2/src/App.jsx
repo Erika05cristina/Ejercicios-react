b6664535ac2db3d9138ed960c3d1d00ff7724e1e
Erika05cristina <evillaq1@est.ups.edu.ec>
2025-01-29 10:53:18 -0500

    fit: 2.9*: La Agenda Telefónica Paso 4

--- Parte2/src/App.jsx
+++ Parte2/src/App.jsx
@@ -1,64 +1,92 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-  ]) 
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
+
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [search, setSearch] = useState("");
 
   const handleNameChange = (event) => {
     console.log(event.target.value);
     setNewName(event.target.value);
-  }
+  };
 
   const handleNumberChange = (event) => {
     console.log(event.target.value);
     setNewNumber(event.target.value);
-  }
+  };
 
+  const handleSearchChange = (event) => {
+    setSearch(event.target.value);
+  };
 
   const addPerson = (event) => {
-    event.preventDefault()
+    event.preventDefault();
 
-    if (persons.some(person => person.name === newName)) {
+    if (persons.some((person) => person.name === newName)) {
       alert(`${newName} is already added to phonebook`);
       return;
     }
-    console.log('button clicked', event.target);
+    console.log("button clicked", event.target);
 
     const newPerson = { name: newName, number: newNumber };
     setPersons([...persons, newPerson]);
-    setNewName('');
-    setNewNumber('');
-  } 
+    setNewName("");
+    setNewNumber("");
+  };
+
+  const personsToShow = persons.filter((person) =>
+    person.name.toLowerCase().includes(search.toLowerCase())
+  );
 
   return (
     <div>
       <h2>Phonebook</h2>
+
+      <div>
+        busqueda <input value={search} onChange={handleSearchChange} />
+      </div>
+
+      <h2>Filtered Contacts</h2>
+      <ul>
+        {personsToShow.map((person) => (
+          <li key={person.id}>
+            {person.name} {person.number}
+          </li>
+        ))}
+      </ul>
+
+      <h3>Add a new</h3>
       <form onSubmit={addPerson}>
         <div>
-          name: <input 
-          value={newName}
-          onChange={handleNameChange}
-          />
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
-        <div>number: <input 
-        value={newNumber}
-        onChange={handleNumberChange}
-        /></div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
-      
+
       <h2>Contacts</h2>
       <ul>
-        {persons.map(person => <li key={person.name}>{person.name} {person.number}</li>)} 
+        {persons.map((person) => (
+          <li key={person.name}>
+            {person.name} {person.number}
+          </li>
+        ))}
       </ul>
+
       
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
