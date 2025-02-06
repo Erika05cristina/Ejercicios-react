d3a772e4d5ab94a528157ff1ff58f53e71100591
Erika05cristina <evillaq1@est.ups.edu.ec>
2025-02-06 05:51:00 -0500

    fit: 3.13: Base de datos de la Agenda Telefónica, paso 1

--- Parte3/index.js
+++ Parte3/index.js
@@ -14,6 +14,27 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
  
+
+const mongoose = require('mongoose')
+
+const password = process.argv[2]
+
+// DO NOT SAVE YOUR PASSWORD TO GITHUB!!
+const url = `mongodb+srv://erika05cristin:${password}@cluster0.s3dzl.mongodb.net/phonebook?retryWrites=true&w=majority`
+  
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+
+
 let persons = [
     { 
       "id": 1,
@@ -37,8 +58,20 @@ let persons = [
     }
 ];
 
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+
 app.get("/api/persons", (request, response) => {
-    response.json(persons);
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
   });
 
 app.get("/info", (request, response) => {
@@ -68,27 +101,43 @@ app.delete("/api/persons/:id", (request, response) => {
     response.status(204).end();
 });
 
-app.post('/api/persons', (request, response) => {
-    const person = request.body;
+// app.post('/api/persons', (request, response) => {
+//     const person = request.body;
 
-    if (!person.name || !person.number) {
-        return response.status(400).json({ error: 'Falta el nombre o el número' });
-    } else if (persons.some(p => p.name === person.name)) {
-        return response.status(400).json({ error: 'El nombre ya existe en la agenda' });
-    }
+//     if (!person.name || !person.number) {
+//         return response.status(400).json({ error: 'Falta el nombre o el número' });
+//     } else if (persons.some(p => p.name === person.name)) {
+//         return response.status(400).json({ error: 'El nombre ya existe en la agenda' });
+//     }
     
-    const newId = Math.floor(Math.random() * 1000);  
+//     const newId = Math.floor(Math.random() * 1000);  
     
-    while (persons.some(p => p.id === newId)) {
-        newId = Math.floor(Math.random() * 1000);   
-    }
+//     while (persons.some(p => p.id === newId)) {
+//         newId = Math.floor(Math.random() * 1000);   
+//     }
 
  
-    const newPerson = { ...person, id: newId }; 
-    persons.push(newPerson);
-    response.json(newPerson);
-});
+//     const newPerson = { ...person, id: newId }; 
+//     persons.push(newPerson);
+//     response.json(newPerson);
+// });
 
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
+  }
+
+  const person = new Person({
+    name: body.content,
+    number: body.content,
+  })
+
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
+})
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
