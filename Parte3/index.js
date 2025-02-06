4549c4398d87d878575eb89ede60005c9360c683
Erika05cristina <evillaq1@est.ups.edu.ec>
2025-02-06 05:59:38 -0500

    fir: 3.14: Base de datos de la Agenda Telefónica, paso 2

--- Parte3/index.js
+++ Parte3/index.js
@@ -33,32 +33,6 @@ const personSchema = new mongoose.Schema({
 
 const Person = mongoose.model('Person', personSchema)
 
-
-
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-];
-
-
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
@@ -83,15 +57,17 @@ app.get("/info", (request, response) => {
 });
 
 app.get("/api/persons/:id", (request, response) => {
-    const id = Number(request.params.id);
-    const person = persons.find((person) => person.id === id);
-  
+  Person.findById(request.params.id)
+  .then(person => {
     if (person) {
-      response.json(person);
+      response.json(person)
     } else {
-      //response.status(404).end();
-      response.send("Person not found");
+      response.status(404).send({ error: 'Person not found' })
     }
+  })
+  .catch(error => {
+    response.status(400).send({ error: 'Invalid ID' })
+  })
 });
 
 app.delete("/api/persons/:id", (request, response) => {
@@ -101,37 +77,17 @@ app.delete("/api/persons/:id", (request, response) => {
     response.status(204).end();
 });
 
-// app.post('/api/persons', (request, response) => {
-//     const person = request.body;
-
-//     if (!person.name || !person.number) {
-//         return response.status(400).json({ error: 'Falta el nombre o el número' });
-//     } else if (persons.some(p => p.name === person.name)) {
-//         return response.status(400).json({ error: 'El nombre ya existe en la agenda' });
-//     }
-    
-//     const newId = Math.floor(Math.random() * 1000);  
-    
-//     while (persons.some(p => p.id === newId)) {
-//         newId = Math.floor(Math.random() * 1000);   
-//     }
-
- 
-//     const newPerson = { ...person, id: newId }; 
-//     persons.push(newPerson);
-//     response.json(newPerson);
-// });
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
 
-  if (body.content === undefined) {
-    return response.status(400).json({ error: 'content missing' })
+  if (!body.name || !body.number) {
+    return response.status(400).json({ error: 'Missing name or number' })
   }
 
   const person = new Person({
-    name: body.content,
-    number: body.content,
+    name: body.name,
+    number: body.number,
   })
 
   person.save().then(savedPerson => {
