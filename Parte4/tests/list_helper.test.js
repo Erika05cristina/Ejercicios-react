fb657400d2a5114c6765036767cea5e3b2dee596
Erika05cristina <evillaq1@est.ups.edu.ec>
2025-02-12 07:46:52 -0500

    fit: 4.4: Funciones Auxiliares y Pruebas Unitarias, paso 2

--- Parte4/tests/list_helper.test.js
+++ Parte4/tests/list_helper.test.js
@@ -1,10 +1,50 @@
-const { test } = require('node:test')
+const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
-test('dummy returns one', () => {
-  const blogs = []
+describe('total likes', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0
+    }
+  ]
 
-  const result = listHelper.dummy(blogs)
-  assert.strictEqual(result, 1)
+  test('when list has only one blog, equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    assert.strictEqual(result, 5)
+  })
+
+  const listWithMultipleBlogs = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f9',
+      title: 'A New Kind of Science',
+      author: 'Stephen Wolfram',
+      url: 'https://www.wolframscience.com/nks/',
+      likes: 7,
+      __v: 0
+    }
+  ]
+
+  test('when list has multiple blogs, equals the sum of likes', () => {
+    const result = listHelper.totalLikes(listWithMultipleBlogs)
+    assert.strictEqual(result, 12) 
+  })
+
+  test('when list is empty, equals 0', () => {
+    const result = listHelper.totalLikes([])
+    assert.strictEqual(result, 0)
+  })
 })
