453a310d60409ff999adf9890914db04cb147a48
Erika05cristina <evillaq1@est.ups.edu.ec>
2025-02-25 09:01:17 -0500

    feat: 5.10: Frontend de la Lista de Blogs, paso 10

--- Parte5/bloglist-frontend/src/App.jsx
+++ Parte5/bloglist-frontend/src/App.jsx
@@ -163,9 +163,11 @@ const App = () => {
         />
       )}
 
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} updateBlog={updateBlog} />
-      ))}
+      {blogs
+        .sort((a, b) => b.likes - a.likes) 
+        .map((blog) => (
+          <Blog key={blog.id} blog={blog} updateBlog={updateBlog} />
+        ))}
     </div>
   );
 };
