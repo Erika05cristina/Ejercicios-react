6da1cf0985a32440bcbc13cdda9aecfa97c4d0a6
Erika05cristina <evillaq1@est.ups.edu.ec>
2025-02-26 09:23:28 -0500

    feat: 5.15: Pruebas de Listas de Blogs, paso 3

--- Parte5/bloglist-frontend/src/tests/Blog.test.jsx
+++ Parte5/bloglist-frontend/src/tests/Blog.test.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { render, screen } from "@testing-library/react";
 import { expect } from "vitest";
 import "@testing-library/jest-dom";
+import { vi } from "vitest";
 import userEvent from '@testing-library/user-event';
 import Blog from '../components/Blog';
+
 
 test('muestra el título y el autor pero no la URL ni los likes por defecto', () => {
   const blog = {
@@ -35,5 +37,26 @@ test('muestra la URL y los likes cuando se hace clic en el botón "View"', async
 
   expect(screen.getByText('http://example.com')).toBeInTheDocument();
   expect(screen.getByText('Likes: 10')).toBeInTheDocument();
+});
+
+test('llama al manejador de likes dos veces cuando se hace clic dos veces en "Like"', async () => {
+  const blog = {
+    title: 'Blog de prueba',
+    author: 'Autor de prueba',
+    url: 'http://example.com',
+    likes: 10
+  };
+  const mockUpdateBlog = vi.fn();
+
+  render(<Blog blog={blog} updateBlog={mockUpdateBlog} deleteBlog={() => {}} user={{ id: '123' }} />);
+
+  const viewButton = screen.getByText('View');
+  await userEvent.click(viewButton);
+
+  const likeButton = screen.getByText('Like');
+  await userEvent.click(likeButton);
+  await userEvent.click(likeButton);
+
+  expect(mockUpdateBlog).toHaveBeenCalledTimes(2);
 });
 
