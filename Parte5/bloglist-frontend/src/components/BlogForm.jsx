f69eeac66b8a151e269cbb291d0acf6ee62993dd
Erika05cristina <evillaq1@est.ups.edu.ec>
2025-02-26 10:48:01 -0500

    feat: 5.16: Pruebas de Listas de Blogs, paso 4

--- Parte5/bloglist-frontend/src/components/BlogForm.jsx
+++ Parte5/bloglist-frontend/src/components/BlogForm.jsx
@@ -1,22 +1,26 @@
 const BlogForm = ({ handleNewBlogSubmit, newBlog, handleNewBlogChange, setFormVisible }) => {
+  const submitHandler = (e) => {
+    e.preventDefault();
+    handleNewBlogSubmit(newBlog); // Verifica que esta función esté siendo llamada
+  };
     return (
       <div>
-        <form onSubmit={handleNewBlogSubmit}>
+        <form onSubmit={handleNewBlogSubmit} data-testid="blog-form">
           <div>
             <label>Title</label>
-            <input type="text" name="title" value={newBlog.title} onChange={handleNewBlogChange} required />
+            <input type="text" name="title" placeholder="Title" value={newBlog.title} onChange={handleNewBlogChange} required />
           </div>
           <div>
             <label>Author</label>
-            <input type="text" name="author" value={newBlog.author} onChange={handleNewBlogChange} required />
+            <input type="text" name="author" placeholder="Author" value={newBlog.author} onChange={handleNewBlogChange} required />
           </div>
           <div>
             <label>URL</label>
-            <input type="text" name="url" value={newBlog.url} onChange={handleNewBlogChange} required />
+            <input type="text" name="url" placeholder="URL"  value={newBlog.url} onChange={handleNewBlogChange} required />
           </div>
           <div>
             <label>Likes</label>
-            <input type="number" name="likes" value={newBlog.likes} onChange={handleNewBlogChange} min="0" />
+            <input type="number" name="likes" placeholder="Likes" value={newBlog.likes} onChange={handleNewBlogChange} min="0" />
           </div>
           <button type="submit">Create Blog</button>
           <button onClick={() => setFormVisible(false)}>Cancel</button>
