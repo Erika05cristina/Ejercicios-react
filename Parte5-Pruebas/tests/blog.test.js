d7179bee3a94e9a7a864ae21acf3ee28445406f9
Erika05cristina <evillaq1@est.ups.edu.ec>
2025-02-27 13:20:02 -0500

    feat: 5.23: Pruebas de Extremo a Extremo de la Lista de Blogs, paso 7

--- Parte5-Pruebas/tests/blog.test.js
+++ Parte5-Pruebas/tests/blog.test.js
@@ -53,70 +53,100 @@ const { test, expect, beforeEach, describe } = require("@playwright/test");
 const TOKEN =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImVyaWthIiwiaWQiOiI2N2MwNjExNDEwZTdhZTI4NjEwOGQ3NTQiLCJpYXQiOjE3NDA2Nzc3MzMsImV4cCI6MTc0MDY4MTMzM30.rcVbQMnnyix_IeFDdQrwfiL5HvpeeJ_myrC5OxRGTxg";
 
-test.describe("When logged in", () => {
-  test.beforeEach(async ({ page }) => {
-    test.setTimeout(60000);
-    await page.goto("http://localhost:5173");
 
-    await page.evaluate((token) => {
-      localStorage.setItem(
-        "loggedUser",
-        JSON.stringify({ token, username: "erika" })
-      );
-    }, TOKEN);
-
-    await page.reload();
-  });
-
-  test("a new blog can be created", async ({ page }) => {
-    test.setTimeout(60000);
-
-    await page.click('button:has-text("Create new blog")');
-
-    await page.waitForSelector('input[name="title"]');
-    await page.waitForSelector('input[name="author"]');
-    await page.waitForSelector('input[name="url"]');
-    await page.waitForSelector('input[name="likes"]');
-
-    await page.fill('input[name="title"]', "Nuevo Blog");
-    await page.fill('input[name="author"]', "erika");
-    await page.fill('input[name="url"]', "https://blog.com");
-    await page.fill('input[name="likes"]', "10");
-    await page.click('button[type="submit"]');
-  });
-
-  test('user can delete a blog', async ({ page }) => {
- 
-    test.setTimeout(60000);
+  test.describe("When logged in", () => {
+    test.beforeEach(async ({ page }) => {
+      test.setTimeout(60000);
+      await page.goto("http://localhost:5173");
   
-    await page.goto('http://localhost:5173');
-    await page.evaluate((token) => {
-      localStorage.setItem('loggedUser', JSON.stringify({ token, username: 'erika' }));
-    }, TOKEN);
-    await page.reload();
-
-    await page.click('button:has-text("Create new blog")');
-    await page.fill('input[name="title"]', 'Blog to Delete');
-    await page.fill('input[name="author"]', 'erika');
-    await page.fill('input[name="url"]', 'https://example.com');
-    await page.fill('input[name="likes"]', '10');
-    await page.click('button[type="submit"]');
-
-    await page.waitForSelector('div.blog-summary');
+      await page.evaluate((token) => {
+        localStorage.setItem(
+          "loggedUser",
+          JSON.stringify({ token, username: "erika" })
+        );
+      }, TOKEN);
   
-    await page.click('button:has-text("View")');
-  
-    await page.waitForSelector('div.blog-details');
-
-    page.on('dialog', async dialog => {
-      expect(dialog.message()).toBe('Are you sure you want to delete this blog?');
-      await dialog.accept();  
+      await page.reload();
     });
   
-    await page.click('button:has-text("Delete")');
-
-    await expect(page.locator('div.blog-summary')).not.toContainText('Blog to Delete');
-  });
-
+    test("a new blog can be created", async ({ page }) => {
+      test.setTimeout(60000);
   
-});
+      await page.click('button:has-text("Create new blog")');
+  
+      await page.waitForSelector('input[name="title"]');
+      await page.waitForSelector('input[name="author"]');
+      await page.waitForSelector('input[name="url"]');
+      await page.waitForSelector('input[name="likes"]');
+  
+      await page.fill('input[name="title"]', "Nuevo Blog");
+      await page.fill('input[name="author"]', "erika");
+      await page.fill('input[name="url"]', "https://blog.com");
+      await page.fill('input[name="likes"]', "10");
+      await page.click('button[type="submit"]');
+    });
+  
+    test('user can delete a blog', async ({ page }) => {
+      test.setTimeout(60000);
+    
+      await page.goto('http://localhost:5173');
+      await page.evaluate((token) => {
+        localStorage.setItem('loggedUser', JSON.stringify({ token, username: 'erika' }));
+      }, TOKEN);
+      await page.reload();
+  
+      await page.click('button:has-text("Create new blog")');
+      await page.fill('input[name="title"]', 'Blog to Delete');
+      await page.fill('input[name="author"]', 'erika');
+      await page.fill('input[name="url"]', 'https://example.com');
+      await page.fill('input[name="likes"]', '10');
+      await page.click('button[type="submit"]');
+  
+      await page.waitForSelector('div.blog-summary');
+    
+      await page.click('button:has-text("View")');
+    
+      await page.waitForSelector('div.blog-details');
+  
+      page.on('dialog', async dialog => {
+        expect(dialog.message()).toBe('Are you sure you want to delete this blog?');
+        await dialog.accept();  
+      });
+    
+      await page.click('button:has-text("Delete")');
+  
+      await expect(page.locator('div.blog-summary')).not.toContainText('Blog to Delete');
+    });
+  
+    test('blogs are sorted by likes', async ({ page }) => {
+      test.setTimeout(60000);
+  
+      await page.click('button:has-text("Create new blog")');
+      await page.fill('input[name="title"]', 'Blog A');
+      await page.fill('input[name="author"]', 'erika');
+      await page.fill('input[name="url"]', 'https://blogA.com');
+      await page.fill('input[name="likes"]', '5');
+      await page.click('button[type="submit"]');
+  
+      await page.click('button:has-text("Create new blog")');
+      await page.fill('input[name="title"]', 'Blog B');
+      await page.fill('input[name="author"]', 'erika');
+      await page.fill('input[name="url"]', 'https://blogB.com');
+      await page.fill('input[name="likes"]', '35');
+      await page.click('button[type="submit"]');
+  
+      await page.waitForSelector('div.blog-summary');
+  
+  
+      const blogsLikes = await page.locator('div.blog-summary')
+        .allTextContents();
+  
+      const likes = blogsLikes.map(blog => {
+        const match = blog.match(/Likes: (\d+)/); 
+        return match ? parseInt(match[1]) : 0;
+      });
+  
+      const sortedLikes = [...likes].sort((a, b) => b - a);
+      expect(likes).toEqual(sortedLikes);
+    });
+  });
