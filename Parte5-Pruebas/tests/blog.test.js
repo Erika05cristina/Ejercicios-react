148535c5507f10565711c81a01723394fab89765
Erika05cristina <evillaq1@est.ups.edu.ec>
2025-02-27 13:15:27 -0500

    feat: 5.21: Pruebas de Extremo a Extremo de la Lista de Blogs, paso 5

--- Parte5-Pruebas/tests/blog.test.js
+++ Parte5-Pruebas/tests/blog.test.js
@@ -48,54 +48,75 @@ const { test, expect, beforeEach, describe } = require("@playwright/test");
 //   });
 // });
 
-describe("When logged in", () => {
-  let token;
-  beforeEach(async ({ page }) => {
+
+
+const TOKEN =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImVyaWthIiwiaWQiOiI2N2MwNjExNDEwZTdhZTI4NjEwOGQ3NTQiLCJpYXQiOjE3NDA2Nzc3MzMsImV4cCI6MTc0MDY4MTMzM30.rcVbQMnnyix_IeFDdQrwfiL5HvpeeJ_myrC5OxRGTxg";
+
+test.describe("When logged in", () => {
+  test.beforeEach(async ({ page }) => {
     test.setTimeout(60000);
     await page.goto("http://localhost:5173");
 
-    await page.fill('input[name="username"]', "erika");
-    await page.fill('input[name="password"]', "erika");
-    await page.click('button[type="submit"]');
+    await page.evaluate((token) => {
+      localStorage.setItem(
+        "loggedUser",
+        JSON.stringify({ token, username: "erika" })
+      );
+    }, TOKEN);
 
-
-    token = await page.evaluate(() => {
-      return localStorage.getItem('token'); 
-    });
-
-    console.log("Token:", token);  
+    await page.reload();
   });
 
   test("a new blog can be created", async ({ page }) => {
     test.setTimeout(60000);
-  
-    await page.fill('input[name="username"]', "erika");
-    await page.fill('input[name="password"]', "erika");
-    await page.click('button[type="submit"]');
-  
-    await page.waitForTimeout(2000);
-  
-    const token = await page.evaluate(() => localStorage.getItem('loggedUser') ? JSON.parse(localStorage.getItem('loggedUser')).token : null);
-  
-    console.log("Token:", token);  
-  
-    if (!token) {
-      throw new Error("Token no encontrado");
-    }
-   
-    await page.goto("http://localhost:5173", {
-      headers: {
-        'Authorization': `Bearer ${token}`,
-      },
-    });
-  
+
+    await page.click('button:has-text("Create new blog")');
+
+    await page.waitForSelector('input[name="title"]');
+    await page.waitForSelector('input[name="author"]');
+    await page.waitForSelector('input[name="url"]');
+    await page.waitForSelector('input[name="likes"]');
+
     await page.fill('input[name="title"]', "Nuevo Blog");
-    await page.fill('input[name="author"]', "Nuevo Autor");
+    await page.fill('input[name="author"]', "erika");
     await page.fill('input[name="url"]', "https://blog.com");
     await page.fill('input[name="likes"]', "10");
     await page.click('button[type="submit"]');
-  
-    await expect(page.locator("div.blog")).toContainText("Nuevo Blog");
   });
-}
-);  
+
+  test('user can delete a blog', async ({ page }) => {
+ 
+    test.setTimeout(60000);
+  
+    await page.goto('http://localhost:5173');
+    await page.evaluate((token) => {
+      localStorage.setItem('loggedUser', JSON.stringify({ token, username: 'erika' }));
+    }, TOKEN);
+    await page.reload();
+
+    await page.click('button:has-text("Create new blog")');
+    await page.fill('input[name="title"]', 'Blog to Delete');
+    await page.fill('input[name="author"]', 'erika');
+    await page.fill('input[name="url"]', 'https://example.com');
+    await page.fill('input[name="likes"]', '10');
+    await page.click('button[type="submit"]');
+
+    await page.waitForSelector('div.blog-summary');
+  
+    await page.click('button:has-text("View")');
+  
+    await page.waitForSelector('div.blog-details');
+
+    page.on('dialog', async dialog => {
+      expect(dialog.message()).toBe('Are you sure you want to delete this blog?');
+      await dialog.accept();  
+    });
+  
+    await page.click('button:has-text("Delete")');
+
+    await expect(page.locator('div.blog-summary')).not.toContainText('Blog to Delete');
+  });
+
+  
+});
