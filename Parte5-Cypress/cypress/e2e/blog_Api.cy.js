3c033ada1de70f24ec97879e514c6559d3474b39
Erika05cristina <evillaq1@est.ups.edu.ec>
2025-02-27 15:53:56 -0500

    feat: 5.18: Pruebas de End To End de la Lista de Blogs, paso 2

--- Parte5-Cypress/cypress/e2e/blog_Api.cy.js
+++ Parte5-Cypress/cypress/e2e/blog_Api.cy.js
@@ -1,12 +1,50 @@
-describe('Blog app', function() {
-  beforeEach(function() {
-    cy.visit('http://localhost:5173')
-  })
+// describe('Blog app', function() {
+//   beforeEach(function() {
+//     cy.visit('http://localhost:5173')
+//   })
 
-  it('Login form is shown', function() {
-    cy.contains('Log in to application')  
-    cy.get('input[name="username"]') 
-    cy.get('input[name="password"]') 
-    cy.get('button').contains('Login') 
-  })
-})
+//   it('Login form is shown', function() {
+//     cy.contains('Log in to application')
+//     cy.get('input[name="username"]')
+//     cy.get('input[name="password"]')
+//     cy.get('button').contains('Login')
+//   })
+// })
+
+describe("Blog app", function () {
+  beforeEach(function () {
+    const user = {
+      name: "villa",
+      username: "villa",
+      password: "villa",
+    };
+    cy.request("POST", "http://localhost:3003/api/users/register", user);
+    cy.visit("http://localhost:5173");
+  });
+
+  it("Login form is shown", function () {
+    cy.contains("Log in to application");
+  });
+
+  describe("Login", function () {
+    it("succeeds with correct credentials", function () {
+      cy.get('input[name="username"]').type("villa");
+      cy.get('input[name="password"]').type("villa");
+      cy.get("button").contains("Login").click();
+
+      cy.contains("villa logged in");
+    });
+
+    it("fails with wrong credentials", function () {
+      cy.get('input[name="username"]').type("villa123");
+      cy.get('input[name="password"]').type("123");
+      cy.get("button").contains("Login").click();
+
+      cy.contains("Invalid username or password").should(
+        "have.css",
+        "color",
+        "rgb(255, 0, 0)"
+      );
+    });
+  });
+});
