36a6315eded9c45559ea7082de40954b5359e957
Erika05cristina <evillaq1@est.ups.edu.ec>
2025-02-27 16:37:41 -0500

    feat: 5.19: Pruebas de End To End de la Lista de Blogs, paso 3

--- Parte5-Cypress/cypress/e2e/blog_Api.cy.js
+++ Parte5-Cypress/cypress/e2e/blog_Api.cy.js
@@ -1,29 +1,33 @@
-// describe('Blog app', function() {
-//   beforeEach(function() {
-//     cy.visit('http://localhost:5173')
-//   })
-
-//   it('Login form is shown', function() {
-//     cy.contains('Log in to application')
-//     cy.get('input[name="username"]')
-//     cy.get('input[name="password"]')
-//     cy.get('button').contains('Login')
-//   })
-// })
-
-describe("Blog app", function () {
+describe("Blog app1", function () {
   beforeEach(function () {
-    const user = {
-      name: "villa",
-      username: "villa",
-      password: "villa",
-    };
-    cy.request("POST", "http://localhost:3003/api/users/register", user);
     cy.visit("http://localhost:5173");
   });
 
   it("Login form is shown", function () {
     cy.contains("Log in to application");
+    cy.get('input[name="username"]');
+    cy.get('input[name="password"]');
+    cy.get("button").contains("Login");
+  });
+});
+
+describe("Blog app", function () {
+  beforeEach(function() {
+    cy.session('villa-session', () => {
+      cy.visit('http://localhost:5173');
+      cy.get('input[name="username"]').type('villa');
+      cy.get('input[name="password"]').type('villa');
+      cy.get("button").contains("Login").click();
+      cy.contains('villa logged in');
+    });
+  });
+
+
+  it('Login form is shown', function() {
+    cy.contains('Log in to application');
+    cy.get('input[name="username"]');
+    cy.get('input[name="password"]');
+    cy.get('button').contains('Login');
   });
 
   describe("Login", function () {
@@ -35,16 +39,41 @@ describe("Blog app", function () {
       cy.contains("villa logged in");
     });
 
-    it("fails with wrong credentials", function () {
-      cy.get('input[name="username"]').type("villa123");
-      cy.get('input[name="password"]').type("123");
-      cy.get("button").contains("Login").click();
+    // it("fails with wrong credentials", function () {
+    //   cy.get('input[name="username"]').type("villa123");
+    //   cy.get('input[name="password"]').type("123");
+    //   cy.get("button").contains("Login").click();
 
-      cy.contains("Invalid username or password").should(
-        "have.css",
-        "color",
-        "rgb(255, 0, 0)"
-      );
+    //   cy.contains("Invalid username or password").should(
+    //     "have.css",
+    //     "color",
+    //     "rgb(255, 0, 0)"
+    //   );
+    // });
+  });
+
+  describe('When logged in', function() {
+    beforeEach(function() {
+      cy.visit('http://localhost:5173'); // Visit after session is set up
+    });
+
+    it('A blog can be created', function() {
+      // Asegura que el botón de "Create new blog" está presente antes de hacer clic
+      cy.contains('Create new blog').should('be.visible').click();
+  
+      // Verifica que el formulario se haya mostrado
+      cy.get('input[name="title"]').should('be.visible');
+  
+      // Llena el formulario
+      cy.get('input[name="title"]').type('Cypress Testing');
+      cy.get('input[name="author"]').type('villa');
+      cy.get('input[name="url"]').type('https://cypress.io');
+  
+      // Envía el formulario
+      cy.get('button').contains('Create').click();
+  
+      // Verifica que el nuevo blog aparece en la lista
+      cy.contains('Cypress Testing villa');
     });
   });
 });
--- Parte5-Cypress/cypress/support/commands.js
+++ Parte5-Cypress/cypress/support/commands.js
@@ -23,3 +23,11 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('login', ({ username, password }) => {
+    cy.request('POST', 'http://localhost:3003/api/users/login', { username, password })
+      .then(({ body }) => {
+        localStorage.setItem('loggedBlogUser', JSON.stringify(body)) // Guarda el token
+        cy.visit("http://localhost:5173"); // Recarga la página para que el usuario esté logueado
+      })
+  })
+  
