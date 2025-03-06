10e05265e132dd69c35fdeb02b37891c11338bb2
Erika05cristina <evillaq1@est.ups.edu.ec>
2025-03-06 09:28:53 -0500

    feat: Ejercicio 6.21

--- Parte6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ Parte6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,21 +1,41 @@
+import { useState } from "react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import anecdoteService from "../services/anecdotes";
+
 const AnecdoteForm = () => {
+  const [content, setContent] = useState("");
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: anecdoteService.create,
+    onSuccess: () => {
+      queryClient.invalidateQueries(["anecdotes"]);
+    },
+  });
 
   const onCreate = (event) => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    console.log('new anecdote')
-}
+    event.preventDefault();
+    if (content.length >= 5) {
+      mutation.mutate(content);
+      setContent("");
+    } else {
+      alert("Anecdote must be at least 5 characters long");
+    }
+  };
 
   return (
     <div>
       <h3>create new</h3>
       <form onSubmit={onCreate}>
-        <input name='anecdote' />
+        <input
+          name="anecdote"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        />
         <button type="submit">create</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default AnecdoteForm
+export default AnecdoteForm;
--- Parte6/query-anecdotes/src/services/anecdotes.js
+++ Parte6/query-anecdotes/src/services/anecdotes.js
@@ -6,5 +6,11 @@ const getAll = async () => {
   const response = await axios.get(baseUrl)
   return response.data
 }
+const create = async (content) => {
+  const object = { content, votes: 0 };
+  const response = await axios.post(baseUrl, object);
+  return response.data;
+};
 
-export default { getAll }
+
+export default { getAll, create }
