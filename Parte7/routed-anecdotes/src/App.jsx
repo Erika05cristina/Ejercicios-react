639ace2ccf87997ab3faf297b96ea5318d60c949
Erika05cristina <evillaq1@est.ups.edu.ec>
2025-03-06 23:04:10 -0500

    feat: Ejercicios 7.1.-7.3.

---
+++ Parte7/routed-anecdotes/src/App.jsx
@@ -0,0 +1,127 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route, Link, useParams, useNavigate } from 'react-router-dom'
+
+const Menu = () => {
+  const padding = { paddingRight: 5 }
+  return (
+    <div>
+      <Link to="/" style={padding}>anecdotes</Link>
+      <Link to="/create" style={padding}>create new</Link>
+      <Link to="/about" style={padding}>about</Link>
+    </div>
+  )
+}
+
+const AnecdoteList = ({ anecdotes }) => (
+  <div>
+    <h2>Anecdotes</h2>
+    <ul>
+      {anecdotes.map(anecdote => (
+        <li key={anecdote.id}>
+          <Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link>
+        </li>
+      ))}
+    </ul>
+  </div>
+)
+
+const Anecdote = ({ anecdotes }) => {
+  const { id } = useParams()
+  const anecdote = anecdotes.find(a => a.id === Number(id))
+
+  if (!anecdote) return <p>Anecdote not found</p>
+
+  return (
+    <div>
+      <h2>{anecdote.content}</h2>
+      <p>by {anecdote.author}</p>
+      <p>has {anecdote.votes} votes</p>
+      <p>For more info, see <a href={anecdote.info}>{anecdote.info}</a></p>
+    </div>
+  )
+}
+
+const About = () => (
+  <div>
+    <h2>About anecdote app</h2>
+    <p>According to Wikipedia:</p>
+    <em>
+      An anecdote is a brief, revealing account of an individual person or an incident...
+    </em>
+    <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
+  </div>
+)
+
+const Footer = () => (
+  <div>
+    Anecdote app for <a href="https://fullstackopen.com/">Full Stack Open</a>.
+  </div>
+)
+
+const CreateNew = ({ addNew }) => {
+  const [content, setContent] = useState('')
+  const [author, setAuthor] = useState('')
+  const [info, setInfo] = useState('')
+  const navigate = useNavigate()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    addNew({
+      content,
+      author,
+      info,
+      votes: 0,
+      id: Math.round(Math.random() * 10000)
+    })
+    navigate('/') 
+  }
+
+  return (
+    <div>
+      <h2>Create a new anecdote</h2>
+      <form onSubmit={handleSubmit}>
+        <div>content <input value={content} onChange={(e) => setContent(e.target.value)} /></div>
+        <div>author <input value={author} onChange={(e) => setAuthor(e.target.value)} /></div>
+        <div>info <input value={info} onChange={(e) => setInfo(e.target.value)} /></div>
+        <button>create</button>
+      </form>
+    </div>
+  )
+}
+
+const App = () => {
+  const [anecdotes, setAnecdotes] = useState([
+    {
+      content: 'If it hurts, do it more often',
+      author: 'Jez Humble',
+      info: 'https://martinfowler.com/bliki/FrequencyReducesDifficulty.html',
+      votes: 0,
+      id: 1
+    },
+    {
+      content: 'Premature optimization is the root of all evil',
+      author: 'Donald Knuth',
+      info: 'http://wiki.c2.com/?PrematureOptimization',
+      votes: 0,
+      id: 2
+    }
+  ])
+
+  return (
+    <Router>
+      <div>
+        <h1>Software anecdotes</h1>
+        <Menu />
+        <Routes>
+          <Route path="/" element={<AnecdoteList anecdotes={anecdotes} />} />
+          <Route path="/create" element={<CreateNew addNew={(a) => setAnecdotes(anecdotes.concat(a))} />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/anecdotes/:id" element={<Anecdote anecdotes={anecdotes} />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  )
+}
+
+export default App
